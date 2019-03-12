0f67ed51e8c1ffd1c51318c353efa4dab946513d
jameshhehir <j.hehir1@nuigalway.ie>
2019-03-12 18:39:17 +0000

    add reset button functionality

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -18,7 +18,7 @@ export class Form extends Component {
     addLead: PropTypes.func.isRequired
   };
 
-  onChange = e => this.setState({ [e.target.name]: e.target.value});
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit = e => {
     e.preventDefault();
@@ -33,74 +33,76 @@ export class Form extends Component {
   };
 
 
-render() {
-  const { name, email, stlFile } = this.state;
+  render() {
+    const { name, email, stlFile } = this.state;
     return (
       <div className="card card-body mt-4 mb-4">
-      <h2>Add STL file to database</h2>
-      <form onSubmit={this.onSubmit}>
+        <h2>Add STL file to database</h2>
+        <form onSubmit={this.onSubmit}>
           <div className="App">
             <div className="form-group">
-            <label>Name</label>
-            <input
-              className="form-control"
-              type="text"
-              name="name"
-              onChange={this.onChange}
-              value={name}
-            />
-          </div>
-          <div className="form-group">
-            <label>Email</label>
-            <input
-              className="form-control"
-              type="email"
-              name="email"
-              onChange={this.onChange}
-              value={email}
-            />
-          </div>
-          <div className="form-group" id="div2">
-                    <label htmlFor="obj-file">
-                        Load STL by file
-                        <br/>
-                        <input type="file"
-                               name="file"
-                               
-                               onChange={(e) => {
-                                   console.log(e.target.files)
-                                   this.setState({
-                                       stlFile: e.target.files[0]
-                                   })
-                               }} 
-                               />
-                    </label>
-                    {this.state.stlFile ?
-                        <STLViewer
-                            onSceneRendered={(element) => {
-                                console.log(element)
-                            }}
-                            sceneClassName="test-scene"
-                            file={this.state.stlFile}
-                            className="obj"
-                            modelColor="#909090"/> : null
+              <label>Name</label>
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                onChange={this.onChange}
+                value={name}
+              />
+            </div>
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                onChange={this.onChange}
+                value={email}
+              />
+            </div>
+            <div className="form-group" id="div2">
+              <label htmlFor="obj-file">
+                Load STL by file
+                        <br />
+                <input type="file"
+                  name="file"
 
-                    }
-                </div>
+                  onChange={(e) => {
+                    console.log(e.target.files)
+                    this.setState({
+                      stlFile: e.target.files[0]
+                    })
+                  }}
+                />
+              </label>
+              {this.state.stlFile ?
+                <STLViewer
+                  onSceneRendered={(element) => {
+                    console.log(element)
+                  }}
+                  sceneClassName="test-scene"
+                  file={this.state.stlFile}
+                  className="obj"
+                  modelColor="#909090" /> : null
+              }
+            </div>
 
-                          <div className="form-group">
-            <button type="reset" className="btn btn-danger" 
-            
-            >
-              Reset
+            <div className="form-group">
+              <button onClick={(e) => {
+                console.log(e.target.files)
+                this.setState({
+                  stlFile: null
+                })
+              }} type="reset" className="btn btn-danger">
+                Reset
             </button>
             </div>
 
-          <div className="form-group">
-            <button type="submit" className="btn btn-primary">
-              Submit
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary">
+                Submit
             </button>
-          </div>
+            </div>
           </div>
         </form>
       </div>
