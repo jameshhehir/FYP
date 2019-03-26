12ce798c58998410d1326a9be6d58c5bea6c1f7d
James Hehir <j.hehir1@nuigalway.ie>
2019-03-26 15:27:07 +0000

    test

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leadmanager.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leadmanager.leadmanager.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -10,4 +10,6 @@ if __name__ == '__main__':
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) 
+        ) from exc
+    execute_from_command_line(sys.argv)
+
