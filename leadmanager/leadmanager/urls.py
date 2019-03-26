e322fa0c54382ae55b4cd9e37f5cf0fca05df839
James Hehir <j.hehir1@nuigalway.ie>
2019-03-26 15:41:00 +0000

    test

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('', include('frontend.urls')),
     path('', include('leads.urls')),
-    path('', include('accounts.urls'))
+    path('', include('accounts.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
