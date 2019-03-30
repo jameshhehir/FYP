62a1b75ae1842b9f5e6e107403f6e1ce93eb2a4b
James Hehir <j.hehir1@nuigalway.ie>
2019-03-30 17:36:33 +0000

    test deployment

--- fyp/fyp/settings.py
+++ fyp/fyp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 't7g$gddxotz%5_gwrt64xf#-bjwr(bk_#10!ur9et_s-*vhqti'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fyp-2019.herokuapp.com']
+ALLOWED_HOSTS = ['fyp-2019.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
     'leads',
     'rest_framework',
     'frontend',
