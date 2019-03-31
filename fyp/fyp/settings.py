0d727198fde1f7e5acb59471ff5b6d675278586c
James Hehir <j.hehir1@nuigalway.ie>
2019-03-31 10:15:45 +0100

    test deployment

--- fyp/fyp/settings.py
+++ fyp/fyp/settings.py
@@ -1,5 +1,6 @@
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,5 +127,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-import django_heroku
+
 django_heroku.settings(locals())
